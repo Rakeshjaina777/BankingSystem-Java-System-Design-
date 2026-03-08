9c12b1cbde386c3d64847d84b79674803c69d73f
Rakesh Jain <rakeshjaina777@gmail.com>
2026-03-08 12:25:28 +0530

    initial project setup - spring boot web app

---
+++ src/main/java/com/rakesh/systemdesign/HelloController.java
@@ -0,0 +1,13 @@
+package com.rakesh.systemdesign;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "System Design app is running!";
+    }
+}
